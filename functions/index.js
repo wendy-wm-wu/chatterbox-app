451cb065ebe5076a1cc0348ce3e15fe23ac8d3a9
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-03-11 21:13:13 -0700

    Edit routes to use Express and aadd a dbschema.js file that provides example data

--- functions/index.js
+++ functions/index.js
@@ -3,43 +3,44 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+const db = admin.firestore();
+
 const express = require('express'); 
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getPosts = functions.https.onRequest((request, response) => {
-  admin.firestore().collection('posts').get()
+app.get('/posts', (req, res) => {
+  db.collection('posts')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then((data) => {
       let posts = [];
       data.forEach(doc => {
-        posts.push(doc.data());
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
-      return response.json(posts);
+      return res.json(posts);
     })
     .catch(err => console.error(err));
 });
 
-exports.createPost = functions.https.onRequest((request, response) => {
-  if (request.method !== 'POST') {
-    return response.status(400).json({ error: 'method not allowed' });
-  }
+app.post('/posts', (req, res) => {
   const newPost = {
-    body: request.body.body,
-    userHandle: request.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
   };
-  admin.firestore()
-    .collection('posts')
-    .add(newPost)
-    .then(doc => {
-      return response.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch(error => {
-      response.status(500).json({ error: 'something went wrong' });
-    });
+    db.collection('posts')
+      .add(newPost)
+      .then(doc => {
+        return res.json({ message: `document ${doc.id} created successfully` });
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'something went wrong' });
+      });
 });
+
+exports.api = functions.https.onRequest(app); 
